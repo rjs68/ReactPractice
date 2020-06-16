8ca267e5116e5ecb3f617e5b056607e3ff1b6d97
Rhys <rhys-stephens@hotmail.co.uk>
2020-06-16 18:18:13 +0100

    Basic React intro including props and state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Rhys', age: 23},
+      { name: 'Dave', age: 27},
+      { name: 'Julia', age: 30}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({persons: [
+      { name: 'Rizzle', age: 23},
+      { name: 'Dave', age: 27},
+      { name: 'Julia', age: 21}
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Football</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
