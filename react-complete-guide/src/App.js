ef7b97b5e4a9ce881721cd45b48175b081bbefc4
Rhys <rhys-stephens@hotmail.co.uk>
2020-06-17 16:58:21 +0100

    Worked with debugging including dev tools and ErrorBoundary

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -50,11 +50,11 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return <Person 
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name} 
+                  age={person.age}
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,8 +2,9 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
+    const rnd = Math.random();
+
     return (
-        // <div className="Person" style={style}>
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
